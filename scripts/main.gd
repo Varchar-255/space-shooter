5ce1e4d5c8aabeeb84943890a4ff499cdc1389e4
José Vieira <josevieira.dev@gmail.com>
2017-11-20 20:08:34 -0300

    Adds pause game

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,3 +7,7 @@ func _ready():
 func _on_Button_pressed():
 	get_tree().change_scene("res://scenes/fases.tscn")
 	pass 
+
+func _on_pause_button_pressed():
+	get_tree().set_pause(!get_tree().is_paused())
+	pass # replace with function body
